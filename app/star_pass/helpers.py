f72b9cacb6c6a4a1b6b18b9f29aa73a0586dda1e
Timothy Hull <timothyhull@gmail.com>
2024-07-03 06:05:24 +0000

    Add print_message method

--- app/star_pass/helpers.py
+++ app/star_pass/helpers.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """ Helper methods for star_pass.py """
 
+# Imports - Python Standard Library
+from typing import Any
+from pprint import pprint as pp
+
 
 class Helpers:
     """ Helpers base class object. """
@@ -14,5 +18,42 @@ class Helpers:
             Returns:
                 None.
         """
+
+        return None
+
+    def print_message(
+            self,
+            message: Any,
+            end: str = '\n',
+            pretty_print: bool = False
+    ) -> None:
+        """ Print messages.
+
+            Args:
+                message (Any):
+                    Pre-formatted content to print.
+
+                end (str):
+                    String appended at the end of the message.  Default
+                    is a new line.  Ignored when pretty_print is True.
+
+                pretty_print (bool):
+                    Display the output using pprint.pprint.  Default is
+                    False.
+
+            Returns:
+                None.
+        """
+
+        # Print formatted output
+        if pretty_print is False:
+            # Standard print
+            print(
+                message,
+                end=end
+            )
+        else:
+            # Pretty Print
+            pp(message)
 
         return None
