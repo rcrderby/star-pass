addfad24f3a28e5dc9489898b0160306241fef50
Timothy Hull <timothyhull@gmail.com>
2024-07-06 02:02:46 +0000

    Remove duplicate code (pylint compliance)

--- app/star_pass/_defaults.py
+++ app/star_pass/_defaults.py
@@ -25,9 +25,6 @@
         )
 """
 
-# Imports - Python Standard Library
-from os import path
-
 # Constants
 # HTTP request configuration
 BASE_HEADERS = {
@@ -44,12 +41,6 @@ BASE_FILE_PATH = 'data'
 # Input data file
 INPUT_FILE_DIR = 'csv'
 INPUT_FILE_EXTENSION = '.csv'
-INPUT_FILE_PATH = path.join(
-    'BASE_FILE_PATH',
-    'INPUT_FILE_DIR',
-    'BASE_FILE_NAME'
-)
-INPUT_FILE = f'{INPUT_FILE_PATH}{INPUT_FILE_EXTENSION}'
 
 # Data file management
 DROP_COLUMNS = 'need_name, start_date, start_time'
@@ -67,9 +58,3 @@ JSON_SCHEMA_SHIFT_FILE = 'amplify.shifts.schema.json'
 # Output data file
 OUTPUT_FILE_DIR = 'json'
 OUTPUT_FILE_EXTENSION = '.json'
-OUTPUT_FILE_PATH = path.join(
-    'BASE_FILE_PATH',
-    'OUTPUT_FILE_DIR',
-    'BASE_FILE_NAME'
-)
-OUTPUT_FILE = f'{OUTPUT_FILE_PATH}{OUTPUT_FILE_EXTENSION}'
