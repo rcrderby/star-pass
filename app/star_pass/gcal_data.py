1fb9a29ab31b47436a602e82183428d90ac0d7d1
Timothy Hull <timothyhull@gmail.com>
2024-08-27 07:30:00 +0000

    Remove `pylint` exception

--- app/star_pass/gcal_data.py
+++ app/star_pass/gcal_data.py
@@ -139,7 +139,6 @@ class GCALData:
 
         return datetime_string
 
-    # pylint: disable-next=dangerous-default-value
     def get_gcal_shift_data(
             self,
             timeMin: str,  # pylint: disable=invalid-name
