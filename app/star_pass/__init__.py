ff84915000a39b9ad51d0e7e74e78b97595d74fc
Timothy Hull <timothyhull@gmail.com>
2024-08-28 08:28:14 +0000

    Add `__version__` string

--- app/star_pass/__init__.py
+++ app/star_pass/__init__.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
-""" star_pass package initiation file. """
+""" star_pass package initialization file. """
+
+# Imports - Local
+from .__version__ import __version__ as version
+
+__version__ = version
