4c0260b0c313675503e24048d2b094f6a7cb0304
Timothy Hull <timothyhull@gmail.com>
2024-09-02 22:25:12 +0000

    Initial commit

---
+++ app/star_pass/_arg_parser.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" CLI argument parser the star_pass application. """
+
+# Imports - Python Standard Library
+import argparse
+
+# Constants
+
+parser = argparse.ArgumentParser()
+parser.parse_args()
