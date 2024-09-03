dea25045b4119e56e59286775397526ce1eb0f9b
Timothy Hull <timothyhull@gmail.com>
2024-09-03 08:48:40 +0000

    Initial refactoring to support `argparse`

--- app/__main__.py
+++ app/__main__.py
@@ -1,58 +1,17 @@
 #!/usr/bin/env python3
 """ Main application script. """
 
-# Imports - Python Standard Library
-from sys import argv
-from typing import Dict
-
 # Imports - Local
 from star_pass.amplify_shifts import CreateShifts
 from star_pass.gcal_data import GCALData
 from star_pass._helpers import Helpers
-from star_pass import _defaults
-
-# Constants
-RUN_MODES = _defaults.RUN_MODES
+from star_pass._arg_parser import parse_cli_arguments
 
 # Initialize helper methods
 helpers = Helpers()
 
-
-# Check for CLI arguments
-def get_cli_args() -> Dict[str, str]:
-    """ Check for CLI arguments.
-
-        Args:
-            None.
-
-        Returns:
-            args (Dict[str, str]):
-                Dict of CLI arguments and their values that map to Dict
-                keys and values, respectively.
-    """
-
-    # Create a Dict to store args
-    cli_args = {}
-
-    if len(argv) > 1:
-        # Slice the file name from the list of arguments
-        arg_list = argv[1:]
-
-        # Parse keys and values from arg_list to add to the 'args' Dict
-        for arg in arg_list:
-            # Split the argument and its value to separate variables
-            arg_key, arg_value = arg.split('=')
-
-            # Remove leading '-' chars and strip outer spaces from 'arg_key'
-            arg_key = arg_key.lstrip('-').strip()
-
-            # Strip outer spaces from 'arg_value'
-            arg_value = arg_value.strip()
-
-            # Add `arg_key` and `arg_value` to the `args` Dict
-            cli_args.update({arg_key: arg_value})
-
-    return cli_args
+# Parse CLI arguments
+cli_args = parse_cli_arguments()
 
 
 # Main application function definition
@@ -62,63 +21,55 @@ def main() -> None:
         Args:
             None.
 
-        Returns None.
+        Returns:
+            None.
 
     """
 
-    # Get CLI arguments
-    cli_args = get_cli_args()
+    return None  # TODO
 
-    # Display help
-    # Add app usage instructions
 
-    # Initially set the run mode to invalid
-    run_mode_valid = False
+def create_amplify_shifts():
+    """ TODO """
 
-    # Determine the validity of the 'mode' argument
-    run_mode = cli_args.get('mode', '').lower()
-    if run_mode in RUN_MODES:
-        # Set the run mode to valid
-        run_mode_valid = True
+    # Run the application in 'create_amplify_shifts' mode
+    # Create output message
+    output_message = (
+        '\n\n** Run mode is "Create Amplify Shifts" **\n'
+    )
 
-    # Run the application in the specified mode
-    if run_mode_valid is True:
+    # Display output message
+    helpers.printer(
+        message=output_message
+    )
 
-        # Run the application in 'create_amplify_shifts' mode
-        if run_mode in ('create_amplify_shifts', 'c'):
-            # Create output message
-            output_message = (
-                '\n\n** Run mode is "Create Amplify Shifts" **\n'
-            )
-            # Create CreateShifts object
-            shifts = CreateShifts(
-                **cli_args
-            )
-            # Create shifts
-            shifts.create_new_shifts()
+    # Create CreateShifts object
+    shifts = CreateShifts(
+        **cli_args
+    )
 
-        # Run the application in 'get_gcal_events' mode
-        if run_mode in ('get_gcal_events', 'g'):
-            # Create output message
-            output_message = (
-                '\n\n** Run mode is "Get Google Calendar Events" **\n'
-            )
-            # Create CreateShifts object
-            GCALData(
-                **cli_args
-            )
+    # Create shifts
+    shifts.create_new_shifts()
 
-    # Display usage instructions and exit if the mode is unset or invalid
-    else:
-        # Create output message
-        output_message = (
-            '\n\n** Invalid or unset "mode" argument **\n'
-        )
+    return None
 
-        # Display output message
-        helpers.printer(
-            message=output_message
-        )
+
+def get_gcal_events():
+    """ TODO """
+    # Run the application in 'get_gcal_events' mode
+    output_message = (
+        '\n\n** Run mode is "Get Google Calendar Events" **\n'
+    )
+
+    # Display output message
+    helpers.printer(
+        message=output_message
+    )
+
+    # Create CreateShifts object
+    GCALData(
+        **cli_args
+    )
 
     return None
 
