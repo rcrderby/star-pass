977abbbafce73c80936d99579f31ffebb78efb8b
Timothy Hull <timothyhull@gmail.com>
2025-03-30 19:17:46 +0000

    Update variable name

--- app/star_pass/_defaults.py
+++ app/star_pass/_defaults.py
@@ -134,7 +134,7 @@ START_TIME_COLUMN = 'start_time'
 KEEP_COLUMNS = f'{START_COLUMN}, duration, slots'
 
 # Amplify shift output formatting
-OUTPUT_VERBOSITY = (
+VERBOSITY_LEVELS = (
     'basic',    # Shift name and number of new shifts
     'simple',   # Basic data plus shift dates and times
     'detailed'  # JSON data with headings
